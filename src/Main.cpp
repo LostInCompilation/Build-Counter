49c4bb1afc7e377322f960342526b4acec6ee098
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2023-04-06 10:13:50 +0200

    Creating framework

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
+#include "BCConfig.h"
+
 int main()
 {
     //uint32_t ui = -1;
     
-    std::cout << "Hello! It's working." << std::endl;
+    std::cout << BC_APP_CMAKE_NAME << " - " << BC_VERSION_STRING << std::endl;
+    std::cout << BC_APP_CMAKE_DESCRIPTION << std::endl;
     
     return 0;
 }
