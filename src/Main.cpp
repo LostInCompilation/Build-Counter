16bc4f2e6c9375e22d8d0eef352f31fa3c538b7b
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2023-04-24 05:11:51 +0200

    Testing

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,8 +6,8 @@ int main()
 {
     //uint32_t ui = -1;
     
-    std::cout << BC_APP_CMAKE_NAME << " - " << BC_VERSION_STRING << std::endl;
-    std::cout << BC_APP_CMAKE_DESCRIPTION << std::endl;
+    //std::cout << BC_APP_CMAKE_NAME << " - " << BC_VERSION_STRING << std::endl;
+    //std::cout << BC_APP_CMAKE_DESCRIPTION << std::endl;
     
     return 0;
 }
